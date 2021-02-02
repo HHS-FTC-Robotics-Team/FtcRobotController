69404bc5d3a8fdac4ccd9583cdadfdb934e16326
asarad39 <55108550+asarad39@users.noreply.github.com>
2021-02-02 14:27:59 -0500

    Updated Threshold values for OpenCV

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/EasyOpenCVExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/EasyOpenCVExample.java
@@ -118,7 +118,7 @@ public class EasyOpenCVExample extends LinearOpMode
 //        static final int REGION_WIDTH = 35;
 //        static final int REGION_HEIGHT = 25;
 //
-//        final int FOUR_RING_THRESHOLD = 150;
+//        final int FOUR_RING_THRESHOLD = 145;
 //        final int ONE_RING_THRESHOLD = 135;
 
         //Personalized for Blue-right or Red-right starting positions
@@ -127,8 +127,8 @@ public class EasyOpenCVExample extends LinearOpMode
         static final int REGION_WIDTH = 35;
         static final int REGION_HEIGHT = 25;
 
-        final int FOUR_RING_THRESHOLD = 150;
-        final int ONE_RING_THRESHOLD = 135;
+        final int FOUR_RING_THRESHOLD = 145;
+        final int ONE_RING_THRESHOLD = 125;
 
         Point region1_pointA = new Point(
                 REGION1_TOPLEFT_ANCHOR_POINT.x,
