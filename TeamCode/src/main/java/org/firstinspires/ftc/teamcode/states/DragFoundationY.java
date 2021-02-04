4f3de7cd1b64a74c3bddf1477000a02ff75ed37c
asarad39 <55108550+asarad39@users.noreply.github.com>
2021-02-04 15:57:03 -0500

    Switched cameras in Teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/states/DragFoundationY.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/states/DragFoundationY.java
@@ -43,6 +43,7 @@ import java.util.Date;
  * or add a @Disabled annotation to prevent this OpMode from being added to the Driver Station
  */
 @Autonomous
+@Disabled
 
 public class DragFoundationY extends OpMode {
     /* Declare OpMode members. */
