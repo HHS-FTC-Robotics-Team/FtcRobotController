7f56a57dd621f7c944731d7e8b09071df2c883ad
Carlo <CarloMehegan@gmail.com>
2021-02-04 17:12:53 -0500

    the release servo flips up a little when vuforia detects a target

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop2021.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop2021.java
@@ -120,6 +120,8 @@ public class Teleop2021 extends LinearOpMode {
 
 
         waitForStart();
+        col.releaseCollector();
+
         while (opModeIsActive()) {
 
 //            if (-gamepad2.right_stick_y > 0) {
@@ -130,7 +132,11 @@ public class Teleop2021 extends LinearOpMode {
 //                lift.rest();
 //            }
 
-            col.releaseCollector(); //Todo: Fix!
+            if (cam2.isTargetVisible()) {
+                col.releaseServoPosition(1); //Todo: Fix!
+            } else {
+                col.releaseServoPosition(0.8);
+            }
 
             if (state == "rotate") {
                 if (d.isBusy() == false) {
