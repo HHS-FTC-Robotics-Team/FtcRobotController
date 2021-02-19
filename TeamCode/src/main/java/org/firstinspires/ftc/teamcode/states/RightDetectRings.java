f2062db8b94bb123ead796235736c50a2308893d
asarad39 <55108550+asarad39@users.noreply.github.com>
2021-02-18 22:19:40 -0500

    Modified and troubleshooted existing red right auto to be more accurate and have proper openCV interpretations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/states/RightDetectRings.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/states/RightDetectRings.java
@@ -206,7 +206,7 @@ public class RightDetectRings extends OurState {
         static final int REGION_HEIGHT = 25;
 
         final int FOUR_RING_THRESHOLD = 145;
-        final int ONE_RING_THRESHOLD = 130;
+        final int ONE_RING_THRESHOLD = 128;
 
         Point region1_pointA = new Point(
                 REGION1_TOPLEFT_ANCHOR_POINT.x,
